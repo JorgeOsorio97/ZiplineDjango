4058ee206dc91963139de04f6e9c2abb4b81a620
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2018-08-26 23:16:37 -0500

    agregar StockDiary app y plantilla index

--- ZiplineDjango/StockDiary/urls.py
+++ ZiplineDjango/StockDiary/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, re_path
+from ShowIndicators import views
 from django.conf.urls import url
-from ZiplineDjango import views
 
+app_name = 'StockDiary'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^stock_diary/', include("StockDiary.urls")),
-    re_path(r'^show_indicators/', include("ShowIndicators.urls")),
+
+    re_path(r'^bestStrategy/$', views.callBestStrategy, name = 'bestStrategy'),
     path('', views.index, name='index'),
 ]
