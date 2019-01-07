2077c0c2c10f0fe242169194580688c6da80a996
JorgeOsorio97 <jorge.valdez.osorio@gmail.com>
2019-01-06 23:18:38 -0600

    primeros avances en visualizador de trabajadores

--- integraRH/views.py
+++ integraRH/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
+import pandas as pd
 
 # Create your views here.
 
@@ -32,8 +33,33 @@ def regristroInicialTrabajador(request):
     return render(request, "integraRH/registroInicialTrabajador.html", context_dict) 
 
 def formularioTrabajador(request):
-    return render(request, "integraRH/formularioTrabajador.html") 
+    context_dict = {'trabajadorCreado' : False}
+    if request.method == 'POST':
+        data = pd.DataFrame([{"Nombre": request.POST['nombre'],
+                        "ApellidoP":request.POST['firstln'],
+                        "ApellidoM": request.POST['secondln'],
+                        "EstadoCivil": request.POST['civil'],
+                        "Conyuge": request.POST['couple'],
+                        "Edad":request.POST['age'],
+                        "RFC": request.POST['rfc'],
+                        "CURP": request.POST['curp']}
+                        ])
+        data.to_csv("trabajador.csv", index = False)
+        context_dict['trabajadorCreado'] = True
+    return render(request, "integraRH/formularioTrabajador.html", context_dict) 
+        
 
 def visualizadorTrabajador(request):
-    return render(request, "integraRH/visualizadorTrabajador.html") 
+    context_dict = {}
+    data = pd.read_csv("trabajador.csv")
+    context_dict['Nombre'] = data['Nombre'][0]
+    context_dict['ApellidoP'] = data['ApellidoP'][0]
+    context_dict['ApellidoM'] = data['ApellidoM'][0]
+    context_dict['EstadoCivil'] = data['EstadoCivil'][0]
+    context_dict['Conyuge'] = data['Conyuge'][0]
+    context_dict['Edad'] = data['Edad'][0]
+    context_dict['RFC'] = data['RFC'][0]
+    context_dict['CURP'] = data['CURP'][0]
+    data_dict = {'data': context_dict}
+    return render(request, "integraRH/visualizadorTrabajador.html", data_dict) 
 
