dbfd699d416503df116be241ef8c9554f31fa256
ThereminB <barucisaac@gmail.com>
2019-02-08 10:34:46 -0600

    Securities.csv

--- ShowIndicators/admin.py
+++ ShowIndicators/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 from .models import Securities, Strategies, Result
 #from .models import Strategies
-from ShowIndicators.models import Securities, Strategies
+#from ShowIndicators.models import Securities, Strategies
 
 # Register your models here.
 @admin.register(Securities)
