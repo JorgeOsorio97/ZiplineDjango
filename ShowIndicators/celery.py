cd7bf90199f3dffa09845dc97e6b85297fd8face
ThereminB <barucisaac@gmail.com>
2019-02-20 17:29:52 -0600

    Celery configuration, tasks test

--- ShowIndicators/celery.py
+++ ShowIndicators/celery.py
@@ -2,12 +2,12 @@ from __future__ import absolute_import, unicode_literals
 import os 
 from celery import Celery 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_mail.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ZiplineDjango.settings.base')
 
 app = Celery('proj')
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
 app.autodiscover_tasks()
 
 app.conf.update(
-    BROKER_URL = 'redis://localhost:6379/0',
+    BROKER_URL = 'sqs://AKIAIXPPXFTW77RSDS7A:FGXzxmBI3dMiE3Em1cTWgnBJyhXol49VgFGV8TOF@'
 )
--- ShowIndicators/tasks.py
+++ ShowIndicators/tasks.py
@@ -1,1 +1,8 @@
-from ZiplineDjango import app
+from celery import Celery
+
+app = Celery('tasks', broker = 'sqs://AKIAIXPPXFTW77RSDS7A:FGXzxmBI3dMiE3Em1cTWgnBJyhXol49VgFGV8TOF@')
+
+@app.task
+def suma(x, y):
+    return x+y
+
